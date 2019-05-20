76dcbc0a33d35c71ef98e589afbb051a0c77b521
sakitkepala <andikapriyotama@gmail.com>
2019-05-21 03:56:17 +0700

    Perbarui schema & resolver untuk authentification

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,9 +1,36 @@
+const { GraphQLScalarType } = require('graphql')
+const moment = require('moment')
+const { User } = require('./models')
+
 const resolvers = {
   Query: {
     test (_, args, context) {
       return 'Hello World!'
     }
-  }
+  },
+  Mutation: {
+    async captureEmail (_, {email}) {
+      const isEmailTaken = await User.findOne({email})
+      if (isEmailTaken) {
+        throw new Error('Email ini sudah dipakai')
+      }
+      const user = await User.create({
+        email,
+        role: 'Owner',
+        status: 'Pending'
+      })
+      return user
+    },
+    async signup (_, {id, firstname, lastname, password}) {},
+    async login (_, {email, password}) {}
+  },
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'Date custom data type',
+    parseValue: (value) => moment(value).toDate(),
+    serialize: (value) => value.getTime(),
+    parseLiteral: (ast) => ast
+  })
 }
 
 module.exports = resolvers
