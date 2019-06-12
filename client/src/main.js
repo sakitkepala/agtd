ebf4e9f09f15153d504195f361a1a58d51b54bf9
sakitkepala <andikapriyotama@gmail.com>
2019-06-12 14:21:43 +0700

    Copas SCSS

--- client/src/main.js
+++ client/src/main.js
@@ -4,7 +4,7 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { onError } from "apollo-link-error"
 import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
-import { enableExperimentalFragmentVariables } from ' graphql-tag'
+// import { enableExperimentalFragmentVariables } from ' graphql-tag'
 import VueApollo from 'vue-apollo'
 import ElementUI from 'element-ui'
 
